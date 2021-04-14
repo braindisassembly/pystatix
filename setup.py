0f323428b10efa7986e21ae157b95b3f43c75bc6
Nathalon <69394316+Nathalon@users.noreply.github.com>
2021-04-14 23:00:38 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import *
 
 kwargs = {
-    "author" : "BrainDisassembly",
-    "author_email" : "braindisassm@gmail.com",
+    "author" : "Nathalon",
     "description" : "statix 1.0.0 [File Statistics Script]",
     "entry_points" : {"console_scripts" : ["statix=statix.statix:main"]},
     "license" : "GPL v3",
