e995f74b9c4f696796eebd7960862f5439905680
braindisassembly <69394316+braindisassembly@users.noreply.github.com>
2021-04-14 06:31:56 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import *
 kwargs = {
     "author" : "BrainDisassembly",
     "author_email" : "braindisassm@gmail.com",
-    "description" : "PyStatix 1.0.0 [File Statistics Script]",
-    "entry_points" : {"console_scripts" : ["pystatix=pystatix.pystatix:main"]},
+    "description" : "Statix 1.0.0 [File Statistics Script]",
+    "entry_points" : {"console_scripts" : ["Statix=Statix.Statix:main"]},
     "license" : "GPL v3",
-    "name" : "pystatix",
-    "packages" : ["pystatix"],
+    "name" : "Statix",
+    "packages" : ["Statix"],
     "version" : "V1.0.0",
 }
 
